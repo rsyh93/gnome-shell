060cbb65f60b9ea95d75dcd536090b8fe47019ca
Alexander Mikhaylenko <exalm7659@gmail.com>
2020-01-21 18:09:56 +0500

    workspaceAnimation: Stop depending on shellwm
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1326

--- js/ui/windowManager.js
+++ js/ui/windowManager.js
@@ -984,6 +984,11 @@ var WindowManager = class {
 
         this._workspaceAnimation =
             new WorkspaceAnimation.WorkspaceAnimationController();
+
+        this._shellwm.connect('kill-switch-workspace', () => {
+            this._workspaceAnimation.cancelSwitchAnimation();
+            this._switchWorkspaceDone();
+        });
     }
 
     _showPadOsd(display, device, settings, imagePath, editionMode, monitorIndex) {
@@ -1608,7 +1613,20 @@ var WindowManager = class {
             return;
         }
 
-        this._workspaceAnimation.animateSwitch(shellwm, from, to, direction);
+        this._switchInProgress = true;
+
+        this._workspaceAnimation.animateSwitch(from, to, direction, () => {
+            this._shellwm.completed_switch_workspace();
+            this._switchInProgress = false;
+        });
+    }
+
+    _switchWorkspaceDone() {
+        if (!this._switchInProgress)
+            return;
+
+        this._shellwm.completed_switch_workspace();
+        this._switchInProgress = false;
     }
 
     _showTilePreview(shellwm, window, tileRect, monitorIndex) {
